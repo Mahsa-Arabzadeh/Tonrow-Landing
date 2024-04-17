5d7cf0167efb2cb667be27ef5fd70bc8db28cecc
Tabasoume <145778331+Tboumpsh@users.noreply.github.com>
2024-04-17 04:12:51 +0330

    [docs]-Complete the component of the map component.
    Description:
    Complete the description and document of the map component in the Map section.
    This section is available in the Figma design.

--- src/components/map/map.js
+++ src/components/map/map.js
@@ -1,7 +1,16 @@
-import domGenerator from "dom-generator";
+import mapGenerator from "dom-generator";
+//
+/**=======================================================================================================================
+ **                                                  mapGenerator
+ *?  This MAP Map makes the sequel.This function of taking a series of inputs and outputs makes it more usable.That has a card and photo.
+ *@param name type
+ *@param name type
+ *@return type
+ *=======================================================================================================================
+ **/
 
 function createMap() {
-  domGenerator({
+  mapGenerator({
     tag: "div",
     attributes: { id: "map" },
     children: [
