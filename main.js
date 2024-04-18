a5309ba4c01d85727986be58701cc00394431794
Mahsa <arabzadehmah@gmail.com>
2024-04-18 18:38:59 +0430

    Fix(orders): fix function orders

--- main.js
+++ main.js
@@ -3,6 +3,6 @@ import createMap from "./src/components/map/index.js";
 import "./src/components/map/index.scss";
 import { whyTonrowGenerator } from "./src/components/WhyTonrow/index.js";
 
-whyTonrowGenerator();
-// header();
-// createMap();
+// whyTonrowGenerator();
+header();
+createMap();
