9145d70f105001df828e12d05b44d89db6ce0384
Tabasoume <145778331+Tboumpsh@users.noreply.github.com>
2024-04-18 20:20:45 +0330

    Feat(buttonComponent): Select different buttons only by changing a variable
    Description:
    Description:
    In this section, I try to build a function that gives the same button to get the same button.
    And include all the design buttons and can be developed.
    This structure is not yet complete and is developing.
    The structure may be completely changed.

--- src/components/Button/button.component.js
+++ src/components/Button/button.component.js
@@ -1,13 +1,30 @@
-import buttonGenerator from "dom-generator";
+import domGenerator from "dom-generator";
 
 undefined;
 function baseButtonGenerator(textContent) {
-  let button = buttonGenerator({
-    tag: "section",
+  let button = domGenerator({
+    tag: "button",
     attributes: { id: "baseButton" },
     properties: { textContent: textContent },
   });
 
-  return button
-  
+  return button;
+}
+
+function changeSize(selector) {
+  let button = document.getElementById("baseButton");
+  switch (selector) {
+    case "small":
+      button.setAttribute("id", "small");
+      break;
+    case "medium":
+      button.setAttribute("id", "medium");
+      break;
+    case "big":
+      button.setAttribute("id", "big");
+      break;
+    case "borderBig":
+      button.setAttribute("id", "borderBig");
+      break;
+  }
 }
