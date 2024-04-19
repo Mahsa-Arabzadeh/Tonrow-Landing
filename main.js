ab2e62012e0b558828ee45cab7c4f2ba9b6d390e
Tabasoume <145778331+Tboumpsh@users.noreply.github.com>
2024-04-19 20:34:34 +0330

    Refactor(bottonComponent):Changing the algorithm and function structure of the button
    Description:
    This is the button creation function. This function creates the button component and supports different buttons according to the figma layout and outside the layout. This algorithm can also be developed.
    
    Docs(buttonComponent):
    Complete detailed information on how functions and inputs work

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
-import header from "./src/components/AppHeader/index.js";
-import headerData from "./src/components/AppHeader/data.js";
 import "./src/components/AppHeader/index.scss";
 import "./src/components/map/index.scss";
-import renderMap from "./src/components/map/index.js";
-// import { whyTonrowGenerator } from "./src/components/WhyTonrow/index.js";
 
-header(headerData);
-renderMap();
+import baseButtonGenerator from "./src/components/Button/buttonComponent.js";
+import buttonsConfig from "./src/components/Button/configButtonComponent.js";
+// import { whyTonrowGenerator } from "./src/components/WhyTonrow/index.js";
+import headerData from "./src/components/AppHeader/data.js";
+import header from "./src/components/AppHeader/index.js";
+import renderMap from "./src/components/map/index.js";
+
+// baseButtonGenerator('jukd',"white", "blue", "black", 170, 180)
---
+++ src/components/Button/configButtonComponent.js
@@ -0,0 +1,21 @@
+let buttonsConfig = [
+  {
+    color: "white",
+    textContent: "jfjf",
+    backgroundColor: "blue",
+    borderColor: "black",
+    imageSrc: "image1.png",
+    width: 120,
+    height: 40,
+  },
+  {
+    color: "black",
+    textContent: "gfbg",
+    backgroundColor: "yellow",
+    borderColor: "red",
+    width: 100,
+    height: 50,
+  },
+];
+
+export default buttonsConfig
