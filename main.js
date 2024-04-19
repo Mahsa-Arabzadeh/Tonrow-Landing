d2f331ef26d988304c4980cab67f094630e8f713
Tabasoume <145778331+Tboumpsh@users.noreply.github.com>
2024-04-19 20:56:35 +0330

    Feat(main.js):imports js file
    Description;
    Due to the name change of each folder, the imported links were sorted.

--- main.js
+++ main.js
@@ -2,20 +2,14 @@ import "./src/components/headerComponent/index.scss";
 import "./src/components/mapComponent/index.scss";
 
 import buttonsConfig from "./src/components/buttonComponent/configButtonComponent.js";
+import baseButtonGenerator from "./src/components/buttonComponent/buttonComponent.js";
 import { getHeaderDataByRole } from "./src/components/headerComponent/validate";
-import baseButtonGenerator from "./src/components/Button/buttonComponent.js";
-import header from "./src/components/headerComponent/headerComponent";
-import renderMap from "./src/components/mapComponent/mapComponent";
-import headerData from "./src/components/AppHeader/data.js";
+import { publicHeaderData } from "./src/components/headerComponent/data.js";
+import header from "./src/components/headerComponent/headerComponent.js";
+import renderMap from "./src/components/mapComponent/mapComponent.js";
 
 // Define the default role
+
 const defaultRole = "public";
+renderMap()
 
-// Get the header data array based on the default role
-const headerData = getHeaderDataByRole(defaultRole);
-
-// Call the handleHeaderError function to handle errors and render the header
-handleHeaderError(headerData);
-
-// Render the map
-renderMap();
