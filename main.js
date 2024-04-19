6b04449c7a937a1d1205a43a3a2d8fa32b7498f3
Ashkan <ashkansalehpoor81@gmail.com>
2024-04-19 18:13:14 +0330

    Chore(main): Update imports and error handling for header component
    Updated import statements in main.js to include the errorHandler.js file and renamed the imported function to handleHeaderError. This change ensures that errors related to the header component are handled properly.

--- main.js
+++ main.js
@@ -1,8 +1,18 @@
 import header from "./src/components/headerComponent/headerComponent";
-import headerData from "./src/components/headerComponent/data";
+import { getHeaderDataByRole } from "./src/components/headerComponent/validate";
+import handleHeaderError from "./src/components/headerComponent/errorHandler"; // Importing the default export from errorHandler.js
 import "./src/components/headerComponent/index.scss";
 import "./src/components/mapComponent/index.scss";
 import renderMap from "./src/components/mapComponent/mapComponent";
 
-header(headerData);
+// Define the default role
+const defaultRole = "public";
+
+// Get the header data array based on the default role
+const headerData = getHeaderDataByRole(defaultRole);
+
+// Call the handleHeaderError function to handle errors and render the header
+handleHeaderError(headerData);
+
+// Render the map
 renderMap();
