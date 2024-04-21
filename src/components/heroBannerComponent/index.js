019929417341fbd927d31550b2f372f28c9852b2
Mahsa <arabzadehmah@gmail.com>
2024-04-21 23:58:59 +0430

    Fix(conflict): in the heroBannerComponent

--- src/components/heroBannerComponent/index.js
+++ src/components/heroBannerComponent/index.js
@@ -2,36 +2,26 @@ import domGenerator from "dom-generator";
 import "./index.scss";
 
 const heroGenerator = document.body.appendChild(
-<<<<<<< HEAD
-
-    domGenerator(
-
-        // start of hero section
-        {
-            tag: "section",
-            attributes: { id: "hero-section" },
-            children: [
-
-                {
-                    tag : "div",
-                    attributes : {id:"hero-banner"}
-                }
-                
-
-            ]
-
-        })
-)
-
-
-=======
   domGenerator(
     // start of hero section
     {
       tag: "section",
       attributes: { id: "hero-section" },
-      children: [{}],
+      children: [
+        {
+          tag: "div",
+          attributes: { id: "hero-banner" },
+        },
+      ],
     }
   )
+
+  //   domGenerator(
+  //     // start of hero section
+  //     {
+  //       tag: "section",
+  //       attributes: { id: "hero-section" },
+  //       children: [{}],
+  //     }
+  //   )
 );
->>>>>>> d2a1e8d170cccb2e5dc1d53cf179afb7f6e15daf
