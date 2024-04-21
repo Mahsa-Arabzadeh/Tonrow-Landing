7c07a01cafc10f82fcef9a6818176ca1a35fa6c1
Alireza-Zeynabi <zeynabialireza@gmail.com>
2024-04-21 17:08:02 +0330

    Feat(heroBannerComponent) : create hero section element
    create hero section element and appendChild in body

--- src/components/heroBannerComponent/index.js
+++ src/components/heroBannerComponent/index.js
@@ -1,2 +1,19 @@
 import domGenerator from "dom-generator";
 import "./index.scss";
+
+
+const heroGenerator = document.body.appendChild(
+
+    domGenerator(
+
+        // start of hero section
+        {
+            tag: "section",
+            attributes: { id: "hero-section" },
+            children: [
+
+
+            ]
+
+        })
+)
