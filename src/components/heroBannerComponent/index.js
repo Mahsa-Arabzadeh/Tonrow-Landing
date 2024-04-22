67801521fdc37bbc68c2e057f4475adaadc5badd
Alireza-Zeynabi <zeynabialireza@gmail.com>
2024-04-22 15:30:21 +0330

    Refactor( heroBannerSection) : Remove extra codes

--- src/components/heroBannerComponent/index.js
+++ src/components/heroBannerComponent/index.js
@@ -3,33 +3,9 @@ import "./index.scss";
 
 
 
+
+
 export default function heroBannerSection() {
-
-    const heroGenerator = document.body.appendChild(
-
-        domGenerator(
-    
-            // start of hero section
-            {
-                tag: "section",
-                attributes: { id: "hero-section" },
-                children: [
-    
-                    {
-                        tag : "div",
-                        attributes : {id:"hero-banner"}
-                    }
-                    
-    
-                ]
-    
-            })
-    )
-
-    
-}
-
-
 const heroGenerator = document.body.appendChild(
   domGenerator(
     // start of hero section
@@ -37,14 +13,18 @@ const heroGenerator = document.body.appendChild(
       tag: "section",
       attributes: { id: "hero-section" },
       children: [
+        // start of hero banner (all image,icon,shasow in hero section)
         {
           tag: "div",
           attributes: { id: "hero-banner" },
+
         },
       ],
+      // end of 
     }
+    // end of hero section
   )
-
 
 );
 
+  }
