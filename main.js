3aa319202934cbd2f595acddc88bb2c481230df0
Tabasoume <145778331+Tboumpsh@users.noreply.github.com>
2024-04-22 21:02:50 +0330

    Refactor(buttonComponent):change structure
    Change structure baseButtonGenerator function.
    this function create button component.

--- main.js
+++ main.js
@@ -1,16 +1,17 @@
 // import "./src/components/heroBannerComponent/index";
 import "./src/components/headerComponent/index.scss";
+import "./src/components/buttonComponent/index.scss";
 import "./src/components/mapComponent/index.scss";
 import "./src/components/whatDoesTonrowDo/index";
 
 import { whyTonrowGenerator } from "./src/components/whyTonrowComponent/whyTonrowComponent";
+// import buttonOptions from "./src/components/buttonComponent/configButtonComponent";
 import baseButtonGenerator from "./src/components/buttonComponent/buttonComponent";
-import buttonOptions from "./src/components/buttonComponent/configButtonComponent";
 import generatorDOMWhatDoesTonrowDo from "./src/components/whatDoesTonrowDo/index";
 import brandIconGenerator from "./src/components/brandTrust/brandTrustComponent";
 import { getHeaderDataByRole } from "./src/components/headerComponent/validate";
 import header from "./src/components/headerComponent/headerComponent";
-import renderMap from "./src/components/mapComponent/mapComponent";
+// import renderMap from "./src/components/mapComponent/mapComponent";
 // import test from "./src/components/whatDoesTonrowDo/index";
 // import test from "./src/components/whatDoesTonrowDo/index";
 
@@ -22,10 +23,23 @@ import renderMap from "./src/components/mapComponent/mapComponent";
 // document.body.appendChild(customButton);
 
 const defaultRole = "safir";
+
+const buttonOptions = {
+  content: "My Button",
+  size: "large",
+  type: "button",
+  attributes: {
+    class: "btn-primary",
+  },
+  dataattributes: {
+    main: "primer",
+  },
+};
+// button
+//! Testing 
 const customButton = baseButtonGenerator(buttonOptions);
 document.body.appendChild(customButton);
 
 generatorDOMWhatDoesTonrowDo();
 
 brandIconGenerator();
-
--- src/components/buttonComponent/buttonComponent.js
+++ src/components/buttonComponent/buttonComponent.js
@@ -5,28 +5,66 @@ import buttonsConfig from "./configButtonComponent";
 /**=======================================================================================================================
  **                                                 baseButtonGenerator
  *?  This is the button creation function. This function creates the button component and supports different buttons according to the figma layout and outside the layout. This algorithm can also be developed.
- *@param buttonOptions {string}
- *@return button {object}
+ *@param buttonOptions {object}
+ *@return button {button}
  *=======================================================================================================================**/
+
 function baseButtonGenerator(buttonOptions) {
   let button = domGenerator({
     tag: "button",
     attributes: { id: "baseButton" },
   });
 
+  // Set the content of the button
   button.textContent = buttonOptions.content;
 
-  Object.keys(buttonOptions).forEach((property) => {
-    if (property !== "content") {
-      button.style[property] = buttonOptions[property];
-    }
+  // Set the type of the button
+  button.type = buttonOptions.type;
+
+  // Set the size of the button
+  button.classList.add(`btn-${buttonOptions.size}`);
+
+  // Disable the button if necessary
+  if (buttonOptions.disabled) {
+    button.disabled = true;
+  }
+
+  if (buttonOptions.anchorlink) {
+    button.href = buttonOptions.anchorlink;
+  }
+
+  // Add attributes to the button
+  Object.keys(buttonOptions.attributes).forEach((attribute) => {
+    button.setAttribute(attribute, buttonOptions.attributes[attribute]);
   });
+
+  // Add data attributes to the button
+  Object.keys(buttonOptions.dataattributes).forEach((dataAttribute) => {
+    button.dataset[dataAttribute] = buttonOptions.dataattributes[dataAttribute];
+  });
+
+  if (buttonOptions.onclick) {
+    button.addEventListener("click", buttonOptions.onclick);
+  }
+
+  // Add a start icon to the button
+  if (buttonOptions.starticon) {
+    const span = document.createElement("span");
+    span.classList.add("btn-icon", "btn-start-icon");
+    span.innerHTML = buttonOptions.starticon;
+    button.insertBefore(span, button.firstChild);
+  }
+
+  // Add an end icon to the button
+  if (buttonOptions.endicon) {
+    const span = document.createElement("span");
+    span.classList.add("btn-icon", "btn-end-icon");
+    span.innerHTML = buttonOptions.endicon;
+    button.appendChild(span);
+  }
 
   return button;
 }
 
 export default baseButtonGenerator;
 
-//todo  TODO:
-// set attribute
-//
