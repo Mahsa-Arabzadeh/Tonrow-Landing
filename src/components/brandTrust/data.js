bf2ce8f04aef9a2d149a1e0c5309a04dc81b91a3
Mahsa <arabzadehmah@gmail.com>
2024-04-22 10:39:45 +0430

    Feat(brandTrust): Install the swiper package
    Add swiper package to package.json dependencies

--- src/components/brandTrust/data.js
+++ src/components/brandTrust/data.js
@@ -4,11 +4,27 @@ export const brandData = [
     hoveredSrc: "../../public/images/refahlogo-hoverd.svg",
   },
   {
-    defaultSrc: "../../public/images/refahDefualt.svg",
-    hoveredSrc: "../../public/images/refahlogo-hoverd.svg",
+    defaultSrc: "../../public/images/digikaladefualt.svg",
+    hoveredSrc: "../../public/images/digikalahovered.svg",
   },
   {
-    defaultSrc: "../../public/images/refahDefualt.svg",
-    hoveredSrc: "../../public/images/refahlogo-hoverd.svg",
+    defaultSrc: "../../public/images/shilalogodefualt.svg",
+    hoveredSrc: "../../public/images/shilalogohovered.svg",
+  },
+  {
+    defaultSrc: "../../public/images/Zikhoraklogodefualt.svg",
+    hoveredSrc: "../../public/images/Zikhoraklogohovered.svg",
+  },
+  {
+    defaultSrc: "../../public/images/cleanfood-defualt.svg",
+    hoveredSrc: "../../public/images/cleanfood-hovered.svg",
+  },
+  {
+    defaultSrc: "../../public/images/OKlogo-defualt.svg",
+    hoveredSrc: "../../public/images/OKlogo-hovered.svg",
+  },
+  {
+    defaultSrc: "../../public/images/sib360logo-defualt.svg",
+    hoveredSrc: "../../public/images/sib360logo-hovered.svg",
   },
 ];
