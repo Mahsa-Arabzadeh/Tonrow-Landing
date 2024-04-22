7bc5cf6ef6fdf9dab3934989c05e402b515f9321
mohammad_rz1 <mohammadrzx148@gmail.com>
2024-04-22 12:05:20 +0300

    Feat(whatDoesTonrowDo): create body
    created a body SEC whatDoesTonrowDo

--- src/components/whatDoesTonrowDo/index.js
+++ src/components/whatDoesTonrowDo/index.js
@@ -16,16 +16,80 @@ function generatorDOMWhatDoesTonrowDo() {
           attributes: { class: "header-what-services-tonrow " },
           children: [
             {
-                tag: "button",
-                attributes: { class: "btn-head-SEC-whatDoesTonrowDo active-btn-header-what-services-tonrow" },
-                properties: { textContent: "درخواست خرید" },
+              tag: "button",
+              attributes: {
+                class:
+                  "btn-head-SEC-whatDoesTonrowDo active-btn-header-what-services-tonrow",
+              },
+              properties: { textContent: "درخواست خرید" },
             },
             {
-                tag: "button",
-                attributes: { class: "btn-head-SEC-whatDoesTonrowDo" },
-                properties: { textContent: "درخواست سفیر" },
+              tag: "button",
+              attributes: { class: "btn-head-SEC-whatDoesTonrowDo" },
+              properties: { textContent: "درخواست سفیر" },
             },
-          ]
+          ],
+        },
+
+
+        {
+          tag: "div",
+          attributes: { class: "body-what-services-tonrow" },
+          children: [
+            {
+              tag: "img",
+              attributes: { src: "public/images/thumbnail-headertext.png" },
+            },
+
+            {
+              tag: "div",
+              attributes: { class: "description-body-SEC-whatDoesTonrowDo" },
+              children: [
+                {
+                    tag:"h2",
+                    properties: { textContent: "ارسال لیست خرید" },
+                },
+                {
+                    tag:"p",
+                    properties: { textContent: "میتوانید برای سازمان یا منزل شخصی خود فقط با مشخص کردن مبدا و مقصد و ارسال لیست های خرید خود, کارها را با سرعت بیشتری انجام دهید و برای دیگر کار هایتان زمانبخرید." },
+                },
+                {
+                    tag:"button",
+                    properties: { textContent:"درخواست خرید"},
+                },
+              ],
+            },
+          ],
+        },
+
+        {
+          tag: "div",
+          attributes: { class: "body-what-services-tonrow" },
+          children: [
+            {
+              tag: "img",
+              attributes: { src: "public/images/thumbnail-headertext.png" },
+            },
+
+            {
+              tag: "div",
+              attributes: { class: "description-body-SEC-whatDoesTonrowDo" },
+              children: [
+                {
+                    tag:"h2",
+                    properties: { textContent: "ارسال لیست خرید" },
+                },
+                {
+                    tag:"p",
+                    properties: { textContent: "میتوانید برای سازمان یا منزل شخصی خود فقط با مشخص کردن مبدا و مقصد و ارسال لیست های خرید خود, کارها را با سرعت بیشتری انجام دهید و برای دیگر کار هایتان زمانبخرید." },
+                },
+                {
+                    tag:"button",
+                    properties: { textContent:"درخواست خرید"},
+                },
+              ],
+            },
+          ],
         },
       ],
     })
