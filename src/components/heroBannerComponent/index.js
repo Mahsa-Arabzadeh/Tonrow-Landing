03490da6b84a4b81f3256fd6e5a6e67864a825dd
Alireza-Zeynabi <zeynabialireza@gmail.com>
2024-04-22 14:52:02 +0330

    fix (heroBannerComponent) fix Wrong changes

--- src/components/heroBannerComponent/index.js
+++ src/components/heroBannerComponent/index.js
@@ -1,6 +1,35 @@
 import domGenerator from "dom-generator";
 import "./index.scss";
 
+
+
+export default function heroBannerSection() {
+
+    const heroGenerator = document.body.appendChild(
+
+        domGenerator(
+    
+            // start of hero section
+            {
+                tag: "section",
+                attributes: { id: "hero-section" },
+                children: [
+    
+                    {
+                        tag : "div",
+                        attributes : {id:"hero-banner"}
+                    }
+                    
+    
+                ]
+    
+            })
+    )
+
+    
+}
+
+
 const heroGenerator = document.body.appendChild(
   domGenerator(
     // start of hero section
@@ -16,12 +45,6 @@ const heroGenerator = document.body.appendChild(
     }
   )
 
-  //   domGenerator(
-  //     // start of hero section
-  //     {
-  //       tag: "section",
-  //       attributes: { id: "hero-section" },
-  //       children: [{}],
-  //     }
-  //   )
+
 );
+
