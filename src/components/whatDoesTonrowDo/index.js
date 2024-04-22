0d713ac8d862beed44d3455e4d0da3b569ca207f
mohammad_rz1 <mohammadrzx148@gmail.com>
2024-04-22 11:46:16 +0300

    Feat(whatDoesTonrowDo): create head
    created a header SEC whatDoesTonrowDo

--- src/components/whatDoesTonrowDo/index.js
+++ src/components/whatDoesTonrowDo/index.js
@@ -2,21 +2,35 @@ import domGenerator from "dom-generator";
 import "./index.scss";
 
 function generatorDOMWhatDoesTonrowDo() {
-    let conteanerGeneratorWhatDoesTonrowDo =
-        document.body.append(
-            domGenerator({
-                tag: "section",
-                attributes: { class: "what-services-tonrow" },
-                children: [
-                    {
-                        tag: "h1",
-                        properties: { textContent: "تــنــرو، چه خدماتی ارائه میدهد؟" },
-                    },
-                ],
-            })
-          
-        )
+  let conteanerGeneratorWhatDoesTonrowDo = document.body.append(
+    domGenerator({
+      tag: "section",
+      attributes: { class: "what-services-tonrow" },
+      children: [
+        {
+          tag: "h1",
+          properties: { textContent: "تــنــرو، چه خدماتی ارائه میدهد؟" },
+        },
+        {
+          tag: "div",
+          attributes: { class: "header-what-services-tonrow " },
+          children: [
+            {
+                tag: "button",
+                attributes: { class: "btn-head-SEC-whatDoesTonrowDo active-btn-header-what-services-tonrow" },
+                properties: { textContent: "درخواست خرید" },
+            },
+            {
+                tag: "button",
+                attributes: { class: "btn-head-SEC-whatDoesTonrowDo" },
+                properties: { textContent: "درخواست سفیر" },
+            },
+          ]
+        },
+      ],
+    })
+  );
 
-    return conteanerGeneratorWhatDoesTonrowDo
+  return conteanerGeneratorWhatDoesTonrowDo;
 }
-export default generatorDOMWhatDoesTonrowDo
+export default generatorDOMWhatDoesTonrowDo;
