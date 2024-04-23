007d8dcf42bd61ff06cee05a6027a7cd9f1585ad
Mahsa <arabzadehmah@gmail.com>
2024-04-23 21:31:00 +0430

    Feat(footerComponent): create rowGenerator function
    Creating a function that creates a row of footer

--- main.js
+++ main.js
@@ -5,6 +5,7 @@
 // import "./src/components/headerComponent/index.scss";
 // import "./src/components/heroBannerComponent/index";
 // import "./src/components/mapComponent/index.scss";
+import rowGenerator from "./src/components/footerComponent/footerComponent";
 
 // import { whyTonrowGenerator } from "./src/components/whyTonrowComponent/whyTonrowComponent";
 // import renderBreakMotor from "./src/components/motorCycleComponent/motorCycleComponent";
@@ -26,17 +27,10 @@
 // heroBannerSection()
 // renderBreakMotor()
 
-
-
-
-
-
 // const defaultRole = "public";
-
 
 // const defaultRole = "safir";
 // renderMap()
-
 
 // const customButton = baseButtonGenerator(buttonOptions);
 // document.body.appendChild(customButton);
@@ -69,3 +63,5 @@
 
 // const headerData = getHeaderDataByRole(defaultRole);
 // handleHeaderError(headerData);
+
+rowGenerator();
--- src/components/footerComponent/footerComponent.js
+++ src/components/footerComponent/footerComponent.js
@@ -1,1 +1,30 @@
+import domGenerator from "dom-generator";
 import "./index.scss";
+
+/**
+ *
+ * @param {rowTitle} rowTitle
+ * @returns {HTMLElement}
+ */
+function rowGenerator(rowTitle) {
+  const rowElement = {
+    tag: "div",
+    attributes: { class: "row-footer" },
+    children: [
+      {
+        tag: "h3",
+        attributes: { class: "row-title" },
+        properties: { textContent: rowTitle },
+      },
+      {
+        tag: "div",
+        attributes: { class: "text-container" },
+        children: [],
+      },
+    ],
+  };
+
+  return rowElement;
+}
+
+export default rowGenerator;
