f1a2b7fe560b47bc87794833b170f8a44c506b30
Mahsa <arabzadehmah@gmail.com>
2024-04-25 10:49:31 +0430

    Add(constants): Add account information and Tonrow address

--- src/JS/constants/constants.js
+++ src/JS/constants/constants.js
@@ -1,6 +1,14 @@
-import domGenerator from "./dom-generator";
+// Email address for contacting Tonrow
+const tonrowEmail = "info@tonrow.mail.com";
 
-export const section = domGenerator({
-  tag: "section",
-  attributes: { class: "section" },
-});
+// Phone number for contacting Tonrow
+const tonrowNumber = 1800;
+
+// Address for Tonrow's location
+const tonrowAddress = "بوشهر، صلح آباد، خیابان صدرا، پشت پمپ بنزین وزیری";
+
+// Ways to contact us constants:
+const tonrowInstagram = ""; // Instagram account for Tonrow
+const tonrowFacebook = ""; // Facebook page for Tonrow
+const tonrowTelegram = ""; // Telegram account for Tonrow
+const tonrowWhatsapp = ""; // Whatsapp number for Tonrow
