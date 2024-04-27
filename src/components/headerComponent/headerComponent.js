b4ec9c482d08c43f319b4da4621771ba95c11a6f
Ashkan <ashkansalehpoor81@gmail.com>
2024-04-27 23:09:50 +0330

    Refactor(header): Remove document.body.appendChild from headerComponent.js
    Removed the direct appending of the header element to the document body within the headerComponent.js file. Instead, refactored the code to return the created header element, allowing for more flexibility in its usage and better separation of concerns.

--- src/components/headerComponent/headerComponent.js
+++ src/components/headerComponent/headerComponent.js
@@ -19,138 +19,136 @@ import "./index.scss";
  * @returns {HTMLElement} - The created header element.
  */
 export default function header(headerData) {
-  // Create the header element and append it to the document body
-  const headerElement = document.body.appendChild(
-    domGenerator({
-      tag: "header",
-      attributes: { id: "header" },
-      children: [
-        {
-          tag: "div",
-          attributes: { class: "header-content" },
-          children: [
-            // Conditional rendering based on window width
-            window.innerWidth > 900
-              ? {
-                  // Container for logo and links
-                  tag: "div",
-                  attributes: { class: "logo-links-container" },
-                  children: [
-                    // Logo element
-                    {
-                      tag: "a",
-                      attributes: {
-                        href: headerData[0].imgLinkAddress,
-                        class: "logo",
+  // Create the header element
+  const headerElement = domGenerator({
+    tag: "header",
+    attributes: { id: "header" },
+    children: [
+      {
+        tag: "div",
+        attributes: { class: "header-content" },
+        children: [
+          // Conditional rendering based on window width
+          window.innerWidth > 900
+            ? {
+                // Container for logo and links
+                tag: "div",
+                attributes: { class: "logo-links-container" },
+                children: [
+                  // Logo element
+                  {
+                    tag: "a",
+                    attributes: {
+                      href: headerData[0].imgLinkAddress,
+                      class: "logo",
+                    },
+                    children: [
+                      // Image for logo
+                      {
+                        tag: "img",
+                        attributes: {
+                          src: headerData[0].imgAddress,
+                          alt: "Logo",
+                        },
                       },
-                      children: [
-                        // Image for logo
+                    ],
+                  },
+                  // Links
+                  {
+                    tag: "div",
+                    attributes: { class: "logo-links" },
+                    children: headerData
+                      .slice(1)
+                      .filter((item) => !item.btnText)
+                      .map((item) => {
+                        return {
+                          tag: "a",
+                          properties: {
+                            textContent: item.linkText,
+                            href: item.LinkAddress ?? "#",
+                          },
+                        };
+                      }),
+                  },
+                ],
+              }
+            : {
+                // Container for sidebar toggle icon
+                tag: "div",
+                attributes: { class: "sidebar-toggle-icon" },
+                children: [
+                  // Image for sidebar toggle
+                  {
+                    tag: "img",
+                    attributes: {
+                      src: "./public/images/menu.png",
+                      alt: "Open Sidebar",
+                      class: "sidebar-icon",
+                    },
+                    eventListeners: {
+                      // Event listener for sidebar toggle
+                      click: () => {
+                        const sidebarElement = document.getElementById("sidebar");
+                        sidebarElement.style.display =
+                          sidebarElement.style.display === "none"
+                            ? "inline-block"
+                            : "none";
+                      },
+                    },
+                  },
+                ],
+              },
+          // Container for buttons
+          {
+            tag: "div",
+            attributes: { class: "buttons-container" },
+            children: headerData
+              .filter((item) => item.btnText !== undefined)
+              .map((item, index) => {
+                // Copy buttonOptions object
+                const buttonOptionsCopy = { ...buttonOptions };
+                if (item.buttonStyles) {
+                  // Apply styles from item.buttonStyles to buttonOptionsCopy
+                  const { buttonStyles } = item;
+                  Object.keys(buttonStyles).forEach((style) => {
+                    buttonOptionsCopy[style] = buttonStyles[style];
+                  });
+                }
+                // Generate button element using baseButtonGenerator
+                return {
+                  tag: baseButtonGenerator(buttonOptionsCopy),
+                  properties: { textContent: item.btnText },
+                  attributes: {
+                    href: item.btnLink || "#", // Set the button href attribute
+                    class: `btn-${index} ${item.public ? 'btn-public' : ''}`, // Add 'btn-public' class if 'public' property is true
+                  },
+                  children: item.btnImage
+                    ? [
                         {
                           tag: "img",
                           attributes: {
-                            src: headerData[0].imgAddress,
-                            alt: "Logo",
+                            src: item.btnImage,
+                            alt: item.btnText,
+                            class: "button-image", // Add a class for styling
                           },
                         },
-                      ],
+                      ]
+                    : [], // If no btnImage, leave children array empty
+                  eventListeners: {
+                    // Navigate to the specified link when the button is clicked
+                    click: () => {
+                      if (item.btnLink) {
+                        window.location.href = item.btnLink;
+                      }
                     },
-                    // Links
-                    {
-                      tag: "div",
-                      attributes: { class: "logo-links" },
-                      children: headerData
-                        .slice(1)
-                        .filter((item) => !item.btnText)
-                        .map((item) => {
-                          return {
-                            tag: "a",
-                            properties: {
-                              textContent: item.linkText,
-                              href: item.LinkAddress ?? "#",
-                            },
-                          };
-                        }),
-                    },
-                  ],
-                }
-              : {
-                  // Container for sidebar toggle icon
-                  tag: "div",
-                  attributes: { class: "sidebar-toggle-icon" },
-                  children: [
-                    // Image for sidebar toggle
-                    {
-                      tag: "img",
-                      attributes: {
-                        src: "./public/images/menu.png",
-                        alt: "Open Sidebar",
-                        class: "sidebar-icon",
-                      },
-                      eventListeners: {
-                        // Event listener for sidebar toggle
-                        click: () => {
-                          const sidebarElement = document.getElementById("sidebar");
-                          sidebarElement.style.display =
-                            sidebarElement.style.display === "none"
-                              ? "inline-block"
-                              : "none";
-                        },
-                      },
-                    },
-                  ],
-                },
-            // Container for buttons
-            {
-              tag: "div",
-              attributes: { class: "buttons-container" },
-              children: headerData
-                .filter((item) => item.btnText !== undefined)
-                .map((item, index) => {
-                  // Copy buttonOptions object
-                  const buttonOptionsCopy = { ...buttonOptions };
-                  if (item.buttonStyles) {
-                    // Apply styles from item.buttonStyles to buttonOptionsCopy
-                    const { buttonStyles } = item;
-                    Object.keys(buttonStyles).forEach((style) => {
-                      buttonOptionsCopy[style] = buttonStyles[style];
-                    });
-                  }
-                  // Generate button element using baseButtonGenerator
-                  return {
-                    tag: baseButtonGenerator(buttonOptionsCopy),
-                    properties: { textContent: item.btnText },
-                    attributes: {
-                      href: item.btnLink || "#", // Set the button href attribute
-                      class: `btn-${index} ${item.public ? 'btn-public' : ''}`, // Add 'btn-public' class if 'public' property is true
-                    },
-                    children: item.btnImage
-                      ? [
-                          {
-                            tag: "img",
-                            attributes: {
-                              src: item.btnImage,
-                              alt: item.btnText,
-                              class: "button-image", // Add a class for styling
-                            },
-                          },
-                        ]
-                      : [], // If no btnImage, leave children array empty
-                    eventListeners: {
-                      // Navigate to the specified link when the button is clicked
-                      click: () => {
-                        if (item.btnLink) {
-                          window.location.href = item.btnLink;
-                        }
-                      },
-                    },
-                  };
-                }),
-            },
-          ],
-        },
-      ],
-    })
-  );
+                  },
+                };
+              }),
+          },
+        ],
+      },
+    ],
+  });
 
   // If the window width is less than or equal to 900px, create the sidebar
   if (window.innerWidth <= 900) {
@@ -159,4 +157,3 @@ export default function header(headerData) {
 
   return headerElement; // Return the created header element
 }
-
