e186c3c982a11a5566bee73e56e1d51718ced317
Mahsa <arabzadehmah@gmail.com>
2024-04-27 17:29:30 +0430

    Refactor(brandTrust): Delete navigation swiper and swiper css navigation and pagination files
    Remove navigation swiper font from root.scss

--- src/components/brandTrust/brandTrustComponent.js
+++ src/components/brandTrust/brandTrustComponent.js
@@ -1,12 +1,9 @@
 // core version + navigation, pagination modules:
 import Swiper from "swiper";
-import { Navigation } from "swiper/modules";
 import domGenerator from "dom-generator";
 import { brandData } from "./data";
 // import Swiper and modules styles
 import "swiper/css";
-import "swiper/css/navigation";
-import "swiper/css/pagination";
 import "./index.scss";
 
 function swiperBrands() {
@@ -14,12 +11,7 @@ function swiperBrands() {
   swiperElement.setAttribute("class", "swiper");
 
   let swiper = new Swiper(".swiper", {
-    modules: [Navigation],
     slidesPerView: "auto",
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
   });
 
   return swiperElement;
@@ -43,26 +35,6 @@ function generateBrandTrust() {
               tag: "div",
               attributes: { class: "swiper-wrapper" },
             },
-            {
-              tag: "div",
-              attributes: { class: "swiper-btn-container" },
-              children: [
-                {
-                  tag: "div",
-                  attributes: { class: "swiper-button-prev" },
-                },
-              ],
-            },
-            {
-              tag: "div",
-              attributes: { class: "swiper-btn-container" },
-              children: [
-                {
-                  tag: "div",
-                  attributes: { class: "swiper-button-next" },
-                },
-              ],
-            },
           ],
         },
       ],
@@ -81,16 +53,13 @@ function generateBrandTrust() {
 
 /**
  * @param {string} - defaultSrc : The default source of images.
- * @param {string} - hoveredSrc : The hovered source of images.
  * @returns {HTMLElement} - this function generate a dom element of brand icons.
  */
 function brandIconGenerator(defaultSrc) {
-  const brandGenerator = document.body.appendChild(
-    domGenerator({
-      tag: "img",
-      attributes: { class: "swiper-slide", src: defaultSrc },
-    })
-  );
+  const brandGenerator = domGenerator({
+    tag: "img",
+    attributes: { class: "swiper-slide", src: defaultSrc },
+  });
 
   return brandGenerator;
 }
