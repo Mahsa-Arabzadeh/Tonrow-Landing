1c1fcee6cd14468fe07d5aa502c7d721c9c203bb
Tabasoume <145778331+Tboumpsh@users.noreply.github.com>
2024-04-28 16:48:03 +0330

    Docs(buttonComponent):edit documentation for buttons component
    Complete explanation of how the buttons work and how the function of the function and its inputs.

--- src/components/buttonComponent/buttonComponent.js
+++ src/components/buttonComponent/buttonComponent.js
@@ -1,12 +1,21 @@
 import domGenerator from "dom-generator";
 import "./index.scss";
 
-/**=======================================================================================================================
- **                                                 baseButtonGenerator
- *?  This is the button creation function. This function creates the button component and supports different buttons according to the figma layout and outside the layout. This algorithm can also be developed.
- *@param buttonOptions {object}
- *@return button {button}
- *=======================================================================================================================**/
+/**
+ * Generates a base button element with optional properties.
+ * @param {Object} options - The options for generating the button.
+ * @param {string} options.content - The text content of the button.
+ * @param {string} [options.size="medium"] - The size of the button ("small", "medium", "large").
+ * @param {string} [options.status="primaryFill"] - The status of the button ("primaryFill", "secondaryFill", "tertiaryFill", "quaternaryFill").
+ * @param {string} [options.type="button"] - The type of the button ("button", "submit", "reset").
+ * @param {string} [options.className=""] - Additional class names for the button.
+ * @param {string} [options.anchorLink="#"] - The href link for the button if it acts as an anchor.
+ * @param {Object} [options.eventListeners={}] - Event listeners to attach to the button.
+ * @param {boolean} [options.disabled] - Whether the button should be disabled.
+ * @param {string} [options.iconStart=""] - The URL of the image icon to display at the start of the button.
+ * @param {string} [options.iconEnd=""] - The URL of the image icon to display at the end of the button.
+ * @returns {HTMLDivElement} - The generated button element.
+ */
 
 function baseButtonGenerator({
   content,
@@ -29,8 +38,8 @@ function baseButtonGenerator({
     eventListeners,
     children: [
       {
-        tag:'img',
-        attributes:{src:iconStart}
+        tag: "img",
+        attributes: { src: iconStart },
       },
       {
         tag: "button",
@@ -41,9 +50,8 @@ function baseButtonGenerator({
         },
       },
       {
-        tag:'img',
-        attributes:{src:iconEnd}
-
+        tag: "img",
+        attributes: { src: iconEnd },
       },
     ],
   });
--- src/components/buttonComponent/configButtonComponent.js
+++ src/components/buttonComponent/configButtonComponent.js
@@ -1,19 +1,34 @@
-import './buttonComponent';
-import './index.scss';
+import "./buttonComponent";
+import "./index.scss";
+
+/**
+ * Options for generating a button element.
+ * @typedef {Object} ButtonOptions
+ * @property {string} content - The text content of the button.
+ * @property {string} [size="medium"] - The size of the button ("small", "medium", "large").
+ * @property {string} [status="primaryFill"] - The status of the button ("primaryFill", "secondaryFill", "tertiaryFill", "quaternaryFill").
+ * @property {string} [type="button"] - The type of the button ("button", "submit", "reset").
+ * @property {string} [class=""] - Additional class names for the button.
+ * @property {string} [anchorLink="#"] - The href link for the button if it acts as an anchor.
+ * @property {Object.<string, Function>} [eventListeners={}] - Event listeners to attach to the button.
+ * @property {boolean} [disabled] - Whether the button should be disabled.
+ * @property {string} [iconStart=""] - The URL of the image icon to display at the start of the button.
+ */
 
 const buttonOptions = {
-  content: "درخواست خرید",
-  size: "large",
-  statues: "primaryFill",
-  type: "button",
-  class: "btn-primary",
-  anchorLink:
-    "https://",
-    eventListeners:{click:()=> console.log("Hi!")},
-    disabled:false,
-    iconStart: "image/...."
+  content: "درخواست خرید", // null or content
+  size: "large", // size include: small , large , medium , extraLarge , extraSmall
+  statues: "primaryFill", // include: primaryOutLine and primaryTextBtn, primaryFill
+  type: "button", // If you do not need to use it | include all type button :submit , ...
+  class: "btn-primary", // null or add className(If you need to class)
+  anchorLink: "https://", // href | If you do not need to use it
+  eventListeners: { click: () => console.log("Hi!") }, // include all events
+  disabled: false, // disabled true or false ? | If you do not need to use it
+  iconStart: "image/....", // include : iconStart and iconLeft | If you do not need to use it
 };
 
 export default buttonOptions;
 
-
+//! WARNING
+//! statues: Different button modes
+//! size: Different button size
