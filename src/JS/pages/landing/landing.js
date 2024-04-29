461613a24cff57b7e07056d7e0507e21ccd6aba4
Mahsa <arabzadehmah@gmail.com>
2024-04-29 20:46:01 +0430

    Add(landing): Add jsdoc comment for the function pageLandingRendering
    I wrote the explanation in the comments where to append

--- src/JS/pages/landing/landing.js
+++ src/JS/pages/landing/landing.js
@@ -2,7 +2,19 @@ import main from "../../../components/mainComponent/main";
 
 document.addEventListener("DOMContentLoaded", pageLandingRendering);
 
+/**
+ * Header, footer and main section are appended to this function and finally it renders the landing page.
+ * @function pageLandingRendering
+ */
 function pageLandingRendering() {
   const body = document.body;
+  //   prepend header here:
+  //   ...
+
+  //   append main section here:
   body.appendChild(main());
+  //   append main section here:
+
+  //   prepend footer here:
+  //   ...
 }
