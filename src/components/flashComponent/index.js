29b88ae271e157ffcce7af4795adf2ead95a2943
Mahsa <arabzadehmah@gmail.com>
2024-05-02 20:47:21 +0430

    Docs(flashComponent): Writing jsdoc for the function flashMaker

--- src/components/flashComponent/index.js
+++ src/components/flashComponent/index.js
@@ -1,6 +1,12 @@
 import domGenerator from "dom-generator";
 import "./index.scss";
 
+/**
+ * Generates a flash maker element with the provided URL.
+ * @param {Object} Flash option : The options object.
+ * @param {string} options.urlFlash  :The URL of the flash.
+ * @returns {HTMLElement} generated flash maker element.
+ */
 function flashMaker({ urlFlash = "" }) {
   const flashMaker = domGenerator({
     tag: "div",
