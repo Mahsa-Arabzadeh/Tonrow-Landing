33626464b703c4c9ff5d9743f5b0f8f7a3d26dc4
Ashkan <ashkansalehpoor81@gmail.com>
2024-05-03 21:39:32 +0330

    Feat: Add reload functionality to landing page
    Implemented a reload mechanism in the landing.js code to automatically reload the page when the window width falls below 900 pixels. This ensures a responsive behavior when transitioning between different screen sizes, providing a smoother user experience.

--- src/JS/pages/landing/landing.js
+++ src/JS/pages/landing/landing.js
@@ -5,15 +5,41 @@ import main from "../../../components/mainComponent/main";
 document.addEventListener("DOMContentLoaded", pageLandingRendering);
 
 /**
- * Header, footer and main section are appended to this function and finally it renders the landing page.
+ * Header, footer, and main section are appended to this function and finally, it renders the landing page.
  * @function pageLandingRendering
  */
 function pageLandingRendering() {
   const body = document.body;
-  //   prepend header here:
+
+  // Function to reload the page
+  function reloadPage() {
+    location.reload(); // Reload the page
+  }
+
+  let hasReloadedOnce = false; // Variable to track if the page has reloaded once
+
+  // Function to handle window resize
+  function handleWindowResize() {
+    // Check if the window width is below 900px
+    if (window.innerWidth < 900) {
+      // Check if the page has not reloaded once
+      if (!hasReloadedOnce) {
+        hasReloadedOnce = true; // Update the flag to indicate the page has reloaded once
+        reloadPage(); // Reload the page
+      }
+    } else {
+      // Reset the flag when the window width goes above 900px
+      hasReloadedOnce = false;
+    }
+  }
+
+  // Add event listener for window resize
+  window.addEventListener("resize", handleWindowResize);
+
+  // prepend header here:
   body.prepend(header());
-  //   append main section here:
+  // append main section here:
   body.appendChild(main());
-  //   append footer here:
+  // append footer here:
   body.appendChild(footerGenerator());
 }
