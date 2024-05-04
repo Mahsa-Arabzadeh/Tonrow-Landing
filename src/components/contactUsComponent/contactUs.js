fe50622979198f305507078f8476768db7847696
Mahsa <arabzadehmah@gmail.com>
2024-05-04 16:31:25 +0430

    Feat(contactUs): set error handling for contact us items
    write jsdoc for contact us function

--- src/components/contactUsComponent/contactUs.js
+++ src/components/contactUsComponent/contactUs.js
@@ -14,36 +14,48 @@ function contactUs() {
    * @property {string} tag : The HTML tag name.
    * @property {Object} attributes : The attributes of the HTML tag.
    */
-  const contactItem = contactItemsData.map((img) => {
-    const tagContact = {
-      tag: "img",
-      attributes: {
-        class: "contact-img",
-        src: `${img.imageContact ?? "/images/forbiddendefaultImage.svg"}`,
-        href: `${img.hrefImg ?? "#"}`,
-      },
-    };
-    return tagContact;
-  });
+  try {
+    for (const item of contactItemsData) {
+      if (
+        !item.hasOwnProperty("imageContact") ||
+        !item.hasOwnProperty("hrefImg")
+      ) {
+        throw Error("invalid data of contact");
+      }
+    }
+    const contactItem = contactItemsData.map((img) => {
+      const tagContact = {
+        tag: "img",
+        attributes: {
+          class: "contact-img",
+          src: `${img.imageContact ?? "/images/forbiddendefaultImage.svg"}`,
+          href: `${img.hrefImg ?? "#"}`,
+        },
+      };
+      return tagContact;
+    });
 
-  const contactUs = domGenerator({
-    tag: "div",
-    attributes: { class: "contactus-section" },
-    children: [
-      {
-        tag: "h2",
-        attributes: { class: "contact-title" },
-        properties: { textContent: "راه های ارتباط با ما " },
-      },
-      {
-        tag: "div",
-        attributes: { class: "contact-items" },
-        children: contactItem,
-      },
-    ],
-  });
+    const contactUs = domGenerator({
+      tag: "div",
+      attributes: { class: "contactus-section" },
+      children: [
+        {
+          tag: "h2",
+          attributes: { class: "contact-title" },
+          properties: { textContent: "راه های ارتباط با ما " },
+        },
+        {
+          tag: "div",
+          attributes: { class: "contact-items" },
+          children: contactItem,
+        },
+      ],
+    });
 
-  return contactUs;
+    return contactUs;
+  } catch (error) {
+    console.log(error.message);
+  }
 }
 
 export default contactUs;
